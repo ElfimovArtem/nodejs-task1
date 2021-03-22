8912f1198353e1695e8ecc350933df9790c99557
Artem Elfimov <elfimov_artem@epam.com>
2021-03-22 18:20:54 +0300

    feat: task 1.3 - some code refactored.

--- src/task3.js
+++ src/task3.js
@@ -1,9 +1,9 @@
 import csv from 'csvtojson';
 import fs from 'fs';
-import paths from './constants';
+import { PATHS } from './constants';
 
-const writeText = fs.createWriteStream(paths.DEFAULT_TEXT);
-fs.createReadStream(paths.DEFAULT_CSV)
+const writeText = fs.createWriteStream(PATHS.DEFAULT_TEXT);
+fs.createReadStream(PATHS.DEFAULT_CSV)
   .pipe(csv())
   .on('data', (data) =>  writeText.write(data))
   .on('error', (er) => console.log(er));
